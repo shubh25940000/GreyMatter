b9ae3d8096e7c5c97606f070655f60e1b3630942
shubh25940000 <shubh2594@gmail.com>
2022-11-11 18:46:41 +0530

    Deeplearning library running fine.

--- src/NeuralNetwork/DeepLearning.py
+++ src/NeuralNetwork/DeepLearning.py
@@ -1,9 +1,9 @@
 import pandas as pd
-import tqdm
+from tqdm import tqdm as tq
 import numpy as np
 import math
-import sympy as sym
-from sympy import *
+import time
+import matplotlib.pyplot as plt
 
 
 class ActivationFunctions:
@@ -16,7 +16,7 @@ class ActivationFunctions:
         :param x: Takes a single value. Not vectorized
         :return:
         """
-        return max(0, x)
+        return np.maximum(0, x)
 
     def sigmoid(self, x):
         """
@@ -76,8 +76,11 @@ class DeepLearning(ActivationFunctions):
         #             y[i] = (y[i] - np.min(y[i])) / (np.max(y[i]) - np.min(y[i]))
         return y
 
-    def activationInput(self, X, W, B):
-        A = np.dot(X, W) + B
+    def activationInput(self, X, W, B=None):
+        if B:
+            A = np.dot(X, W) + B
+        else:
+            A = np.dot(X, W)
         return A
 
     def activationOutput(self, activation, activationInput, derivative=None):
@@ -119,7 +122,7 @@ class DeepLearning(ActivationFunctions):
         i = 0
         c = 0
         for i in range(actual.shape[0]):
-            if predicted[i][0] == actual[i][0]:
+            if predicted[i] == actual[i]:
                 c += 1
         return str(round(((c / actual.shape[0]) * 100), 2)) + "%"
 
@@ -138,6 +141,11 @@ class Layer(DeepLearning):
         self.input_ = input_
         self.bias = bias
 
+    def encode(self, x):
+        edit = np.zeros(10)
+        edit[x] = 1
+        return edit
+
     def output(self):
         t = self.type_
         if str(t).lower() == "input":
@@ -152,63 +160,91 @@ class Layer(DeepLearning):
 
 
 class Model(DeepLearning, ActivationFunctions):
-    def __init__(self, layers, learningRate, epochs, actual):
+    def __init__(self, layers, learningRate, epochs, actual, X_test = None, y_test = None):
         super().__init__()
         self.layers = layers
         self.epochs = epochs
         self.actual = actual
         self.learningRate = learningRate
+        self.X_test = X_test
+        self.y_test = y_test
 
-    def train(self):
+    def plotErrors(self, figure, plot, epoch, error):
+        plot = plot
+        new_y = error
+        plot.set_xdata(range(0, epoch + 1))
+        plot.set_ydata(new_y)
+        plt.plot(range(0, epoch + 1), error)
+        # figure.canvas.draw()
+        plt.pause(0.5)
+        figure.canvas.flush_events()
+
+    def train(self, EvaluateOnTest = None):
         np.random.seed(seed=42)
         output = self.layers[-1]
         Weights = []
         Biases = []
-        ActivationInput = []
-        ActivationOutput = []
+        ####Initiate graph:
+        graphx = range(1, 0 + 1)
+        graphy = range(0)
+        plt.ion()
+        # here we are creating sub plots
+        figure, ax = plt.subplots(figsize=(6, 4))
+        line1, = ax.plot(graphx, graphy)
+        plt.title("Error vs Epoch", fontsize=20)
+        # setting x-axis label and y-axis label
+        plt.xlabel("X-axis")
+        plt.ylabel("Y-axis")
+        predictedOut = None
+        input_layer = self.layers[0]
+        n_input_features = input_layer.shape[1]
+        for j in self.layers[1:-1]:
+            ####Initializing random weights and bias
+            W = self.vectorNormalize(np.random.randn(n_input_features, j[1]))
+            Weights.append(W)
+            n_input_features = W.shape[1]
+        W = self.vectorNormalize(np.random.randn(n_input_features, output[1]))
+        Weights.append(W)
+        print("Weights are initilized")
+        E = []
         for i in range(self.epochs):
-            X = self.layers[0]
-            if i == 0:
+            print("Epoch = %d" % i)
+            kk = 0
+            OutPutConsolidated = []
+            input_layer = self.layers[0]
+            for inp in tq(input_layer):
                 ####Forward pass
-                print("Forward pass in progress")
+                ActivationInput = []
+                ActivationOutput = []
+                X = np.atleast_2d(inp)
                 n_input_features = X.shape[1]  # 784
-                n_input_rows = X.shape[0]  # 60000
+                p = 0
                 for j in self.layers[1:-1]:
-                    ####Initializing random weights and bias
-                    W = self.vectorNormalize(np.random.randn(n_input_features, j[1]))
-                    B = self.vectorNormalize(np.random.randn(n_input_rows, 1))
-                    hiddeninput_ = self.activationInput(X, W, B)
+                    W = Weights[p]
+                    hiddeninput_ = self.activationInput(X, W)
                     ActivationInput.append(hiddeninput_)
                     k = self.activationOutput(j[0], hiddeninput_)
                     ActivationOutput.append(k)
                     X = k  # 60000X700
-                    n_input_features = X.shape[1]
-                    n_input_rows = X.shape[0]
-                    Weights.append(W)
-                    Biases.append(B)
-                W = self.vectorNormalize(np.random.randn(n_input_features, output[1]))
-                B = self.vectorNormalize(np.random.randn(n_input_rows, 1))
-                hiddeninput_ = self.activationInput(X, W, B)
+                    p = p + 1
+                #                     if kk == 0:
+                #                         print(X)
+                W = Weights[p]
+                hiddeninput_ = self.activationInput(X, W)
                 ActivationInput.append(hiddeninput_)
-                Weights.append(W)
-                Biases.append(B)
                 outputCalculated = self.activationOutput(output[0], hiddeninput_)
                 ActivationOutput.append(outputCalculated)
-                print("Forward pass completed")
+                #                 if kk == 0:
+                #                     print(outputCalculated)
 
-
-            else:
                 ####BackProp
-                print("Back Propogation in progress")
-                print("Epoch = %d" % i)
-                eR = []
-                #                 outputCalculated = None
                 n = -1
-                print("Error = " + str(np.sum((outputCalculated - self.actual) ** 2)))
-                #                     plt.plot(eR.append(np.sum((outputCalculated - self.actual) ** 2)))
-
+                X = np.atleast_2d(inp)
                 layer = len(self.layers[:0:-1]) - 1
-                C_ = (outputCalculated - self.actual)  #### 60000X10
+                C_ = (outputCalculated - self.actual[kk])  #### 60000X10
+                #                 if kk == 0:
+                #                     print(C_**2)
+                #                     print(self.actual[kk])
                 ####Initialize D:
                 D = []
                 D.append(C_ * self.activationOutput(activation=self.layers[n][0],
@@ -217,7 +253,6 @@ class Model(DeepLearning, ActivationFunctions):
                 Weights[n] = Weights[n] - (self.learningRate * ActivationOutput[n - 1].T.dot(D[0]))  ####100,10
                 x = 0
                 for j in self.layers[-2:0:-1]:
-                    print("Updating Weights and Biases for layer: %d" % layer)
                     delta = D[-1].dot(Weights[n].T)
                     delta = delta * self.activationOutput(activation=j[0],
                                                           activationInput=ActivationOutput[n - 1],
@@ -225,58 +260,84 @@ class Model(DeepLearning, ActivationFunctions):
                     D.append(delta)
 
                     if layer == 1:
-                        Weights[n - 1] = Weights[n - 1] - self.learningRate * self.layers[0].T.dot(D[x + 1])
+                        Weights[n - 1] = Weights[n - 1] - (self.learningRate * X.T.dot(D[x + 1]))
                     else:
-                        Weights[n - 1] = Weights[n - 1] - self.learningRate * ActivationOutput[n - 2].T.dot(D[x + 1])
+                        Weights[n - 1] = Weights[n - 1] - (self.learningRate * ActivationOutput[n - 2].T.dot(D[x + 1]))
 
                     #                         D.append(delta)
                     n = n - 1
                     layer = layer - 1
                     x = x + 1
+                OutPutConsolidated.append(outputCalculated)
+                kk = kk + 1
+            finalOutput = np.vstack(OutPutConsolidated)
 
-                ActivationInput = []
-                ActivationOutput = []
-                numberL = 0
-                X = self.layers[0]
-                for j in self.layers[1:-1]:
-                    W = Weights[numberL]
-                    B = Biases[numberL]
+            #             print("Accuracy = %s" % self.getAccuracy(self.decodeOutput(finalOutput),
+            #                                                      self.decodeOutput(self.actual)))
+            print("Error = " + str(np.sum((finalOutput - self.actual) ** 2)))
+            E.append(np.sum((finalOutput - self.actual) ** 2))
+            self.plotErrors(figure, line1, i, E)
 
-                    hiddeninput_ = self.activationInput(X, W, B)
-                    ActivationInput.append(hiddeninput_)
-                    k = self.activationOutput(j[0], hiddeninput_)
-                    ActivationOutput.append(k)
-                    X = k  # 60000X700
-                    n_input_features = X.shape[1]
-                    n_input_rows = X.shape[0]
-                    numberL = numberL + 1
-                W = Weights[-1]
-                B = Biases[-1]
-                hiddeninput_ = self.activationInput(X, W, B)
+            # predictedOut = self.predict(input_layer, Weights, self.actual)
+            print("Accuracy on train dataset = " + str(self.getAccuracy(self.decodeOutput(finalOutput), self.decodeOutput(self.actual))))
+            if EvaluateOnTest == True:
+                print("Accuracy on test dataset = " + str(
+                    self.getAccuracy(self.predict(self.X_test, Weights), self.decodeOutput(self.y_test))))
+        return Weights
+
+    def predict(self, X, Weights):
+        OutPutConsolidated = []
+        for inp in X:
+            ####Forward pass
+            ActivationInput = []
+            ActivationOutput = []
+            X = np.atleast_2d(inp)
+            n_input_features = X.shape[1]  # 784
+            p = 0
+            for j in self.layers[1:-1]:
+                W = Weights[p]
+                hiddeninput_ = self.activationInput(X, W)
                 ActivationInput.append(hiddeninput_)
-                outputCalculated = self.activationOutput(output[0], hiddeninput_)
+                k = self.activationOutput(j[0], hiddeninput_)
+                ActivationOutput.append(k)
+                X = k  # 60000X700
+                p = p + 1
+            #                     if kk == 0:
+            #                         print(X)
+            W = Weights[p]
+            hiddeninput_ = self.activationInput(X, W)
+            ActivationInput.append(hiddeninput_)
+            outputCalculated = self.activationOutput(output[0], hiddeninput_)
+            #             outputCalculated = outputCalculated/np.max(outputCalculated)
+            ActivationOutput.append(outputCalculated)
+            OutPutConsolidated.append(np.argmax(outputCalculated))
+        finalOutput = np.vstack(OutPutConsolidated)
 
-                ActivationOutput.append(outputCalculated)
-
-                X = self.layers[0]
-
-                print("Accuracy = %s" % self.getAccuracy(self.decodeOutput(outputCalculated),
-                                                         self.decodeOutput(self.actual)))
-        return outputCalculated, Weights, Biases
+        # print("Accuracy = " + str(self.getAccuracy(finalOutput, self.decodeOutput(y))))
+        return finalOutput
 
 
 if __name__ == "__main__":
 
-    train = pd.read_csv("DeepLearning/MNIST/mnist_train.csv")
-    X = train.drop("label", axis=1)
-    y = train["label"]
-    X = X.to_numpy()
-    y = y.to_numpy()
-    X = X / np.max(X)
-    inputLayer = Layer(type_ = "input", input_ = X).output()
-    hidden1 = Layer(type_ = "hidden", n_neurons=100, activation = "RELU").output()
-    hidden2 = Layer(type_="hidden", n_neurons=100, activation = "RELU").output()
-    output = Layer(type_ = "output", n_neurons=10, activation = "RELU").output()
+    l = Layer()
+    def preprocessing(input):
+        input["label"] = input["label"].apply(lambda x: l.encode(x))
+        X = input.drop("label", axis=1)
+        y = input["label"]
+        X = X.to_numpy()
+        y = y.to_numpy()
+        y = np.vstack(y)
+        X = X / np.max(X)
+        return X, y
+    train = pd.read_csv("/Users/shubhamchoudhury/Documents/Research/DeepLearning/MNIST/mnist_train.csv")
+    test = pd.read_csv("/Users/shubhamchoudhury/Documents/Research/DeepLearning/MNIST/mnist_test.csv")
+
+    X, y = preprocessing(train)
+    X_test, y_test = preprocessing(test)
+    inputLayer = Layer(type_="input", input_=X).output()
+    hidden1 = Layer(type_="hidden", n_neurons=100, activation="Sigmoid").output()
+    hidden2 = Layer(type_="hidden", n_neurons=100, activation="Sigmoid").output()
+    output = Layer(type_="output", n_neurons=10, activation="Sigmoid").output()
 
     layers = [inputLayer, hidden1, hidden2, output]
-    outputPred, Weights, Biases = Model(layers = layers, learningRate=0.08, epochs=5, n_iter=4, actual = y).train()
+    Weights = Model(layers=layers, learningRate=0.01, epochs=24, actual=y, X_test=X_test, y_test=y_test).train(EvaluateOnTest=True)
